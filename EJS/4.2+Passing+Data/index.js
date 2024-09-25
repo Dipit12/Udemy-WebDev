7983d0f14adf111d4423f5eee18c7a84ef569ccc
Dipit12 <dipitmadan@gmail.com>
2024-09-25 14:43:41 +0530

    EJS completed

--- EJS/4.2+Passing+Data/index.js
+++ EJS/4.2+Passing+Data/index.js
@@ -6,11 +6,13 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
 app.post("/submit", (req, res) => {
   const numLetters = req.body["fName"].length + req.body["lName"].length;
-  res.render("solution.ejs", { numberOfLetters: numLetters });
+  res.render("index.ejs", { numberOfLetters: numLetters });
 });
 
 app.listen(port, () => {
